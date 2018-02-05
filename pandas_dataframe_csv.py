eca9b8a3f64a94040e3356263bb8ea5228e2c06d
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-02-05 12:49:54 +0200

    Update pandas_dataframe_csv.py

--- pandas_dataframe_csv.py
+++ pandas_dataframe_csv.py
@@ -1,7 +1,7 @@
 # Machine learning the IRIS FLOWER set
 
 import pandas as pd
-
+import matplotlib.pyplot as plt                                                       # new
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
@@ -13,3 +13,7 @@ print '-----------'
 print dataset.describe()
 print '-----------'
 print dataset.groupby('class').size()
+
+#plot each feature separately
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)     # new
+plt.show()                                                                            # new
