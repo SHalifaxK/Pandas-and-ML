70d10a9d9c490d03d99c5f990a2024d7e2824950
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-02-22 15:24:55 +0200

    Create data_read_csv_matplot.py

---
+++ data_read_csv_matplot.py
@@ -0,0 +1,31 @@
+#import dummy TESLA share data and manipulating it to suit for analysis
+
+
+#imports
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
+#set matplotlib style so the grapf will look slitly better, from matplotlib import style
+style.use('ggplot')
+
+#read the Tesla data from a csv to pandas dataframe
+data = pd.read_csv('C:/Users/kantomaa/TSLA.csv')
+
+#making a 100 moving average, takes last 100 observations and calculates the average to a specified column
+#make a new column..min_periods=0 to have some values also in the beginning
+data['100ma']=data['Adj Close'].rolling(window=100, min_periods=0).mean()
+
+#making sublots using matplotlib, syntax for subplot2grid matplotlib.pyplot.subplot2grid(shape, loc, rowspan=1, colspan=1, **kwargs)
+ax1 = plt.subplot2grid((6,1),(0,0),rowspan=5,colspan=1)
+ax2 = plt.subplot2grid((6,1),(5,0),rowspan=5,colspan=1,sharex=ax1)
+
+#plotting the data, data.index is the date
+#also setting the index is just simply data.set_index['****']
+ax1.plot(data.index, data['Open'])
+ax1.plot(data.index, data['100ma'])
+ax2.plot(data.index, data['Close'])
+
+plt.show()
+print data.tail(20)
